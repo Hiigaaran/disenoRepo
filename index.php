c7a1474c9da1821a71924b9f17ae53d793bda72e
Hiigaaran <simo.moralesg@gmail.com>
2016-06-28 17:20:10 -0400

    base proyecto php

---
+++ index.php
@@ -0,0 +1,20 @@
+<link href="maquetaLogin.css" type="text/css" rel="stylesheet"/>
+<script src="jscriptLogin.js"></script>
+<meta charset="utf-8" />
+<div class="login-page">
+  <div class="form">
+    <form class="register-form">
+      <input type="text" placeholder="Nombre Usuario"/>
+      <input type="password" placeholder="Contraseña"/>
+      <input type="text" placeholder="Correo Electrónico"/>
+      <button>create</button>
+      <p class="message">Ya estás registrado? <a href="#">Entrar</a></p>
+    </form>
+    <form class="login-form">
+      <input type="text" placeholder="Nombre Usuario" name="usuario"/>
+      <input type="password" placeholder="Contraseña" name="clave"/>
+      <button name="OK" onClick="OK">Entrar</button>
+      <p class="message">No estás registrado? <a href="#">Crear cuenta</a></p>
+    </form>
+  </div>
+</div>

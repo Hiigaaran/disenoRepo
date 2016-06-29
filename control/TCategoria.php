c7de55ab11ae5ccb2a2bc9edc899ffc496f02804
Xanfles <gonzalezpacheco.jonathan@gmail.com>
2016-06-29 14:25:10 -0400

    Controladores para Categoria y sucursal

---
+++ control/TCategoria.php
@@ -0,0 +1,53 @@
+<?php
+require_once("../negocio/producto.php");
+if(isset($_POST["id_cat"]) && $_POST["id_cat"]!="")
+{ $id_cat=$_POST["id_cat"];}
+if(isset($_POST["nombre"]) && $_POST["nombre"]!="")
+{ $nombre=$_POST["nombre"];}
+
+if(isset($_POST["OK"]) && $_POST["OK"]=="Insertar")
+{ //Trigger insercion
+  $objCategoria= new Categoria();//Instancia
+  $objCategoria->Categoria($id_cat, $nombre);
+  $resul=$objCategoria->insertarCategoria();
+  if($resul!="") header("Location:../Vision/View_Categoria.php");
+  else 
+  { echo "<script language='javascript'>alert('ERROR: DATA COULD NOT BE SAVED');window.location='../Vision/View_Categoria.php'</script>";  
+  }
+  
+}
+if(isset($_POST["OK1"]) && $_POST["OK1"]=="Modificar")
+{ //Trigger insercion
+  $objCategoria= new Categoria();//Instancia
+  $objCategoria->Categoria($id_cat, $nombre);
+  $resul=$objCategoria->modificarCategoria();
+   if($resul!="") header("Location:../Vision/View_Categoria.php");
+  else 
+  { echo "<script language='javascript'>alert('ERROR: DATA COULD NOT BE SAVED');window.location='../Vision/View_Categoria.php'</script>";  
+  }
+}
+if(isset($_POST["OK2"]) && $_POST["OK2"]=="Eliminar")
+{ //Trigger insercion
+  $objCategoria= new Categoria();//Instancia
+  $objCategoria->setId_cat($id_cat);//a memoria
+  $resul=$objCategoria->eliminarCategoria($id_cat);
+   if($resul!="") header("Location:../Vision/View_Categoria.php");
+  else 
+  { echo "<script language='javascript'>alert('ERROR: DATA COULD NOT BE SAVED');window.location='../Vision/View_Categoria.php'</script>";  
+  }
+}
+if(isset($_POST["OK"]) && $_POST["OK"]=="Buscar")
+{ //Trigger insercion
+  $objCategoria= new Categoria();//Instancia
+  $objCategoria->setId_cat($id_cat);//a memoria
+  $vector=$objCategoria->buscarCategoria($id_cat);
+  return $vector;
+}
+if(isset($_POST["OK"]) && $_POST["OK"]=="Listar")
+{ //Trigger insercion
+  $objCategoria= new Categoria();//Instancia
+  $matrix=$objCategoria->listarCategoria();
+  return $matrix;
+}
+
+?>
---
+++ control/TSucursal.php
@@ -0,0 +1,57 @@
+<?php
+require_once("../negocio/producto.php");
+if(isset($_POST["id_suc"]) && $_POST["id_suc"]!="")
+{ $id_suc=$_POST["id_suc"];}
+if(isset($_POST["direccion"]) && $_POST["direccion"]!="")
+{ $direccion=$_POST["direccion"];}
+if(isset($_POST["ciudad"]) && $_POST["ciudad"]!="")
+{ $ciudad=$_POST["ciudad"];}
+if(isset($_POST["region"]) && $_POST["region"]!="")
+{ $region=$_POST["region"];}
+
+if(isset($_POST["OK"]) && $_POST["OK"]=="Insertar")
+{ //Trigger insercion
+  $objSucursal= new Sucursal();//Instancia
+  $objSucursal->Sucursal($id_suc, $direccion, $ciudad, $region);
+  $resul=$objSucursal->insertarSucursal();
+  if($resul!="") header("Location:../Vision/View_Sucursal.php");
+  else 
+  { echo "<script language='javascript'>alert('ERROR: DATA COULD NOT BE SAVED');window.location='../Vision/View_Sucursal.php'</script>";  
+  }
+  
+}
+if(isset($_POST["OK1"]) && $_POST["OK1"]=="Modificar")
+{ //Trigger insercion
+  $objSucursal= new Sucursal();//Instancia
+  $objSucursal->Sucursal($id_suc, $direccion, $ciudad, $region);
+  $resul=$objSucursal->modificarSucursal();
+   if($resul!="") header("Location:../Vision/View_Sucursal.php");
+  else 
+  { echo "<script language='javascript'>alert('ERROR: DATA COULD NOT BE SAVED');window.location='../Vision/View_Sucursal.php'</script>";  
+  }
+}
+if(isset($_POST["OK2"]) && $_POST["OK2"]=="Eliminar")
+{ //Trigger insercion
+  $objSucursal= new Sucursal();//Instancia
+  $objSucursal->setId_suc($id_suc);//a memoria
+  $resul=$objSucursal->eliminarSucursal($id_suc);
+   if($resul!="") header("Location:../Vision/View_Sucursal.php");
+  else 
+  { echo "<script language='javascript'>alert('ERROR: DATA COULD NOT BE SAVED');window.location='../Vision/View_Sucursal.php'</script>";  
+  }
+}
+if(isset($_POST["OK"]) && $_POST["OK"]=="Buscar")
+{ //Trigger insercion
+  $objSucursal= new Sucursal();//Instancia
+  $objSucursal->setId_suc($id_suc);//a memoria
+  $vector=$objSucursal->buscarSucursal($id_suc);
+  return $vector;
+}
+if(isset($_POST["OK"]) && $_POST["OK"]=="Listar")
+{ //Trigger insercion
+  $objSucursal= new Sucursal();//Instancia
+  $matrix=$objSucursal->listarSucursal();
+  return $matrix;
+}
+
+?>

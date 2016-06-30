3f0e655e486218bf4ab6ce635cb580d36a13b96b
jordansilva1990 <jordan.silva@hotmail.es>
2016-06-30 15:46:20 -0400

    view_categorias

---
+++ vision/View_Categoria.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	
+	<body>
+		<div id="page-wrapper">
+			<!--Trabajen Aqui!!-->  
+  			<div class="jumbotron">
+    			<h1>Categorias en Sistema</h1>
+    			
+  			</div>
+  						<!--Trabajen Aqui!!-->  
+    	</div>
+    </body>
+</html>
+
+<?php
+	include('master.php');
+	 $objConex= new Conexion();
+ 	 $objConex->abrirConexion();
+	 $sql="SELECT * FROM CATEGORIA";
+	 $clie=mysql_query($sql) or die ("Problema en conexion  a Tabla Cliente");
+	 
+?>

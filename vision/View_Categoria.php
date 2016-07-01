86a9dc02a9d4ee6a4a1bbf8c1e42afbfcb32651c
Xanfles <gonzalezpacheco.jonathan@gmail.com>
2016-07-01 00:01:59 -0400

    Tags php reparados

--- vision/View_Categoria.php
+++ vision/View_Categoria.php
@@ -1,76 +1,67 @@
 <!DOCTYPE html>
 <html>
-		<body>
+    <?php
+        include('master.php');
+        include("../datos/Conexion.php");
+    ?>
+	<body>
 		
-			<!--Trabajen Aqui!!--> 
+		<!--Trabajen Aqui!!--> 
             
-                <?php
-				include('master.php');
-                include("../datos/Conexion.php");
-				
-				echo "<div id='page-wrapper'>";
-				echo "<div class='container-fluid' >";
-				echo "<div id='subviewPlaceHolder_id'>";
-				echo "<h2 class='page-header'>Categorias en Sistema</h2>";
-				echo "<div class='col-lg-6'>";
-				echo "<ol class='breadcrumb'><li><i class='fa fa-dashboard'></i>  <a href='index.html'>Dashboard					                      </a>
-                    </li>
-                    <li class='active'>
-                        <i class='fa fa-calculator'></i> Categorias
-						
-                    </li>
-                </ol>";
+        <?php
+			echo "<div id='page-wrapper'>";
+			echo "<div class='container-fluid' >";
+			echo "<div id='subviewPlaceHolder_id'>";
+			echo "<h2 class='page-header'>Categorias en Sistema</h2>";
+			echo "<div class='col-lg-6'>";
+			echo "<ol class='breadcrumb'><li><i class='fa fa-dashboard'></i>  <a href='index.html'>Dashboard					                      </a>
+                </li>
+                <li class='active'>
+                    <i class='fa fa-calculator'></i> Categorias
+			    </li>
+            </ol>";
 				
 				
-				$objConex= new Conexion();
- 	 			$objConex->abrirConexion();
-	 			$sql="SELECT * FROM categoria";
-	 			$cat=mysql_query($sql) or die ("Problema en conexion  a Tabla Categoria");
-				echo "<div class='table-responsive'>";
-				echo "<table class='table table-hover table-striped'>";
-				echo	"<thead>";
-                echo           "<tr>";
-                echo                "<th>Codigo</th>";
-                echo                "<th>Nombre</th>";
-                echo            "</tr>";
-                echo        "</thead>";
-                echo        "<tbody>";
-                while($matrix=mysql_fetch_row($cat))
-  			  { echo "<tr>";
-   				echo "<td>".$matrix[0]."</td>";
-				echo "<td>".$matrix[1]."</td>";			
-				echo "</tr>";
-  						}                       
-                echo       "</tbody>";
-                echo    "</table>";
-                echo "</div>";
-				?>
+			$objConex= new Conexion();
+ 	 		$objConex->abrirConexion();
+	 		$sql="SELECT * FROM categoria";
+	 		$cat=mysql_query($sql) or die ("Problema en conexion  a Tabla Categoria");
+			echo "<div class='table-responsive'>";
+			echo "<table class='table table-hover table-striped'>";
+			echo	"<thead>";
+            echo           "<tr>";
+            echo                "<th>Codigo</th>";
+            echo                "<th>Nombre</th>";
+            echo            "</tr>";
+            echo        "</thead>";
+            echo        "<tbody>";
+            while($matrix=mysql_fetch_row($cat))
+    		{ echo "<tr>";
+   			  echo "<td>".$matrix[0]."</td>";
+			  echo "<td>".$matrix[1]."</td>";			
+			  echo "</tr>";
+  			}                       
+            echo       "</tbody>";
+            echo    "</table>";
+            echo "</div>";
+		?>
                 
-                    
-                    
-            </div>    
-
-                <div class="table-responsive">
-                    <table class="table table-hover table-striped">
-                        <thead>
-                            <tr>
-                                <th>Codigo</th>
-                                <th>Nombre</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>/index.html</td>
-                                <td>1265</td>                                
-                            </tr>
-                          
-                        </tbody>
-                    </table>
-                </div>
-
-            </div>
-  			</div>
-  						<!--Trabajen Aqui!!-->  
-    	</div>
+        <div class="table-responsive">
+            <table class="table table-hover table-striped">
+                <thead>
+                    <tr>
+                        <th>Codigo</th>
+                        <th>Nombre</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>/index.html</td>
+                        <td>1265</td>                                
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+  		<!--Trabajen Aqui!!-->
     </body>
 </html>
